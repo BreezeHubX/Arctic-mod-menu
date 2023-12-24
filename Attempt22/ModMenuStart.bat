d4de3282f6c399529dd68ab957791d4d7c0f6ce9
BreezeHubX <96991216+BreezeHubX@users.noreply.github.com>
2023-12-24 16:36:23 +0100

    Update ModMenuStart.bat

--- Attempt22/ModMenuStart.bat
+++ Attempt22/ModMenuStart.bat
@@ -1,12 +1,12 @@
 start "vid.mp4" "C:\Users\%username%\Downloads\Arctic\UI-Generator\vid.mp4"
 timeout /t 6 /nobreak
-start Spam.bat " Spam.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\Spam.bat"
+start "Spam.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\Spam.bat"
 timeout /t 1 /nobreak
-start msg.bat "msg.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\msg.bat"
+start "Spam.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\Spam.bat"
 timeout /t 1 /nobreak
-start msg.bat "msg.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\msg.bat"
+start "Spam.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\Spam.bat"
 timeout /t 1 /nobreak
-start msg.bat "msg.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\msg.bat"
+start "Spam.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\Spam.bat"
 timeout /t 1 /nobreak
-start msg.bat "msg.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\msg.bat"
-start GetCommand.bat "GetCommand.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\GetCommand.bat"
+start "Spam.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\Spam.bat"
+start"GetCommand.bat" "C:\Users\%username%\Downloads\Arctic\UI-Generator\GetCommand.bat"
